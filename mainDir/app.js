cb5c633ff53b9e4d002e6a47ef9b638e6fa218d4
Tsallagov Vladimir <vovasso550@rambler.ru>
2020-12-16 13:46:33 +0300

    16.12.2020

--- mainDir/app.js
+++ mainDir/app.js
@@ -7,6 +7,8 @@ const greeting = require("./greeting");
 //import module with date and greeting
 const User = require("./user");
 //import module with user constructor
+let greeting1 = require("./greetingName");
+
 
 
 http.createServer(function (request, response){
@@ -20,11 +22,19 @@ http.createServer(function (request, response){
         +"\r\n"
         +vladimir.sayHi()
     );
+
+
     //server answer goes to url answer
 }).listen(3000, "127.0.0.1", function(){
     console.log("Сервер начал прослушивание запросов, порт 3000"
                 + greeting.date
+                +`Hello ${greeting1.name}`
     );
     //params to listen
     //console log goes down
 });
+
+let greeting2 = require("./greetingName");
+greeting2.name = "Bob";
+console.log(`Hello ${greeting2.name}`);
+console.log(`Hello ${greeting1.name}`);
